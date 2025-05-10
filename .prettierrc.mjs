0ca2cc1cb0ee2f0b0ee0b1b602afa104e2bbab41
Shinya Fujino <shf0811@gmail.com>
2025-05-11 01:02:13 +0900

    Add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+export default {
+	useTabs: true,
+	plugins: ["prettier-plugin-astro"],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
+
+
+
